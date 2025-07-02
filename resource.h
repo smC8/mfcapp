da3bb54d4082c88591f33548fd064e66161dac7e
smC8 <38998898+smC8@users.noreply.github.com>
2025-07-02 10:44:41 +0530

    Create resource.h

---
+++ resource.h
@@ -0,0 +1,6 @@
+
+#define IDD_SUMAPP_DIALOG               102
+#define IDR_MAINFRAME                   128
+#define IDC_EDIT1                       1000
+#define IDC_EDIT2                       1001
+#define IDC_EDIT3                       1002
